19c6785176a9bacbc992d2c2d64b92152f963160
Danuj15 <er.anujjain.me@gmail.com>
2022-10-12 12:51:03 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,13 @@
+
+SEARCH_TYPE = "SUBJECT"
+SEARCH_VALUE = "project file"
+MAIL_READ_PORT = 993
+EMAIL_ID = 'nitin.s@zecdata.com'
+APP_PASSWORD = 'llmocfyufmttaqxa'
+MAIL_READ_HOST = 'imap.gmail.com'
+
+AWS_ACCESS_KEY_ID = 'AKIAW5NYCNSTNBQXNLH3'
+AWS_SECRET_ACCESS_KEY = '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'
+KEY = "tmp"
+BUCKET_NAME = "emailfiels04102022"
+region_name ='eu-west-1'
